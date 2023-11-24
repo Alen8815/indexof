dd462236dc9f8d9a454fe633daa2d2a6c6ab40ca
Alen8815 <alen.pineda536@ist17dejulio.edu.ec>
2023-11-24 15:14:37 -0500

    prueba completa

--- src/indexof/ejercicio.java
+++ src/indexof/ejercicio.java
@@ -4,10 +4,35 @@
  */
 package indexof;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author user
  */
 public class ejercicio {
-    
+      public static void main(String[] args) {
+        // Solicitar la letra a buscar
+        String letraABuscar = JOptionPane.showInputDialog("Ingrese la letra a buscar:");
+
+        // Verificar que solo se haya ingresado una letra
+        if (letraABuscar.length() != 1) {
+            JOptionPane.showMessageDialog(null, "Por favor, ingrese solo una letra.");
+            return;
+        }
+
+        // Solicitar la palabra
+        String palabra = JOptionPane.showInputDialog("Ingrese la palabra a comprobar:");
+
+        // Buscar la letra en la palabra
+        int indice = palabra.indexOf(letraABuscar.charAt(0));
+
+        // Verificar si la letra está en la palabra
+        if (indice != -1) {
+            JOptionPane.showMessageDialog(null, "La letra '" + letraABuscar + "' está en la palabra.");
+        } else {
+            JOptionPane.showMessageDialog(null, "La letra '" + letraABuscar + "' no está en la palabra.");
+        }
+    }
 }
+
